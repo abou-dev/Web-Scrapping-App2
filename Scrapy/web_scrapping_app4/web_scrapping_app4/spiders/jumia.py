d362e7fbc82107b18b8b78b1f147013b8e2729f8
bassirou-loum <82762223+bassirou-loum@users.noreply.github.com>
2023-07-23 11:54:43 +0000

    mettre les donnée dans la base

--- Scrapy/web_scrapping_app4/web_scrapping_app4/spiders/jumia.py
+++ Scrapy/web_scrapping_app4/web_scrapping_app4/spiders/jumia.py
@@ -1,6 +1,7 @@
 
 import json
 import unicodedata
+from pymongo import MongoClient
 from scrapy import Request, Spider
 
 from web_scrapping_app4.items import ProduitItem
@@ -24,7 +25,7 @@ class JumiaSpider(Spider):
             designation = article.css('a.core div.name::text').extract_first()
             if designation:
                 designation = unicodedata.normalize('NFKD', designation).encode('ASCII', 'ignore').decode('utf-8')
-                designation = designation.replace('\u2013', '-').replace('\u2033', '"')
+                designation = designation.replace('\u2013', '-').replace('\u2033', '').replace('\"',"")
             image = article.css('a.core img::attr(data-src)').extract_first()
             prix = article.css('a.core div.prc::text').extract_first()
             prix = prix.replace(' ', '').strip() if prix else None
@@ -40,7 +41,7 @@ class JumiaSpider(Spider):
             self.items.append(item)
 
 
-        # Récupérer les données des pages 2 à 5
+        # Récupérer les données des pages 2 à 25
         for page in range(2, 25):
             next_page_url = f'https://www.jumia.sn/telephone-tablette/?page={page}#catalog-listing'
             yield Request(url=next_page_url, callback=self.parse)
@@ -50,5 +51,16 @@ class JumiaSpider(Spider):
         # Enregistrement des données dans un fichier JSON à la fermeture de l'araignée
         data = [dict(item) for item in self.items]
 
-        with open('jumia.json', 'w') as json_file:
-            json.dump(data, json_file, indent=4)
+        #with open('jumia.json', 'w') as json_file:
+        #    json.dump(data, json_file, indent=4)
+
+        # Connexion à MongoDB
+        client = MongoClient('mongodb://localhost:27017/')
+        db = client['resultat_db']
+        collection = db['jumia']
+
+        # Insérer les données dans la collection MongoDB
+        collection.insert_many(data)
+
+        # Fermer la connexion à MongoDB
+        client.close()
--- Scrapy/web_scrapping_app4/web_scrapping_app4/spiders/spider_prix.py
+++ Scrapy/web_scrapping_app4/web_scrapping_app4/spiders/spider_prix.py
@@ -1,10 +1,9 @@
 import json
+
+from pymongo import MongoClient
 from scrapy import Request, Spider
 
 from web_scrapping_app4.items import ProduitItem
-
-
-
 
 class DakarMarketSpider(Spider):
     name = "dakarmarket"
@@ -12,21 +11,19 @@ class DakarMarketSpider(Spider):
     site_name = 'DakarMarket'
     items = []
 
-
     def start_requests(self):
         for url in self.start_urls:
             yield Request(url=url, callback=self.parse)
-
 
     def parse(self, response):
         listItems = response.css('div.card')
         for item in listItems:
             designation = item.css('a::attr(title)').get()
             designation = designation.encode('ascii', 'ignore').decode('utf-8')
-            image = item.css('img::attr(data-src)').get()
-            prix = item.css('span.fs-4::text').get()
+            designation = designation.replace('\"', "")
+            image = item.css('img.img-fluid::attr(src)').extract_first()
+            prix = item.css('div.card-footer::text').get()
             prix = prix.replace('\n', '').replace('\u202f', '').replace('\u00a0', '').strip() if prix else None
-
 
             comparateur_item = ProduitItem()
             comparateur_item['designation'] = designation
@@ -34,20 +31,27 @@ class DakarMarketSpider(Spider):
             comparateur_item['prix'] = prix
             comparateur_item['site'] = self.site_name
 
-
             self.items.append(comparateur_item)
-
 
         # Récupérer les données des pages 2 à 27
         for page in range(2, 28):
             next_page_url = f'https://dakarmarket.sn/categorie/telephone-portable?page={page}'
             yield Request(url=next_page_url, callback=self.parse)
 
-
     def closed(self, reason):
         # Enregistrement des données dans un fichier JSON à la fermeture de l'araignée
         data = [dict(item) for item in self.items]
 
+        #with open('dakarmarket.json', 'w') as json_file:
+        #    json.dump(data, json_file, indent=4)
 
-        with open('dakarmarket.json', 'w') as json_file:
-            json.dump(data, json_file, indent=4)
+        # Connexion à MongoDB
+        client = MongoClient('mongodb://localhost:27017/')
+        db = client['resultat_db']
+        collection = db['dakarmarket']
+
+        # Insérer les données dans la collection MongoDB
+        collection.insert_many(data)
+
+        # Fermer la connexion à MongoDB
+        client.close()
